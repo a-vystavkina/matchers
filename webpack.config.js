2a0621c0a55fec539be1bb5d2343db953a941831
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2020-02-18 18:16:10 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,12 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: 'css-loader',
-        test: /\.ts$/,
-        use: 'ts-loader',
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
